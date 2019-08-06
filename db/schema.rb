b8a81e5260f903a2eb6aed183a9c669ec0ed7548
KaranFrieds <karanrajsinghranawat@gmail.com>
2019-08-06 06:59:26 +0530

    little touch up

--- db/schema.rb
+++ db/schema.rb
@@ -120,11 +120,11 @@ ActiveRecord::Schema.define(version: 2019_07_31_115251) do
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email"
     t.string "phone_number"
     t.string "alternate_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
