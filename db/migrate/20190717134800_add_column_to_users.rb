95c18c76db5852baf7f7569257592d5d8366837f
KaranFrieds <karanrajsinghranawat@gmail.com>
2019-08-06 01:02:45 +0000

    Update 20190717134800_add_column_to_users.rb

--- db/migrate/20190717134800_add_column_to_users.rb
+++ db/migrate/20190717134800_add_column_to_users.rb
@@ -1,7 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :email, :string
-    add_column :users, :phone_number, :string
-    add_column :users, :alternate_number, :string
+    
   end
 end
